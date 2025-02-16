5b7aadc28e279d38711a8d52b683274e838f553c
Kuldeep kaushik <45338143+kuldeep101990@users.noreply.github.com>
2025-02-16 16:05:57 +0530

    Update HibernateInheritanceExample.java

--- src/main/java/criteriaQuery/HibernateInheritanceExample.java
+++ src/main/java/criteriaQuery/HibernateInheritanceExample.java
@@ -14,6 +14,7 @@ import org.hibernate.transform.Transformers;
 
 import java.util.Date;
 import java.util.List;
+import java.util.function.Consumer;
 
 public class HibernateInheritanceExample {
 
@@ -59,40 +60,58 @@ public class HibernateInheritanceExample {
                 // Commit the transaction
                 tx.commit();
 
-                // Create a Criteria object
-                Criteria criteria = session.createCriteria(Person.class);
 
                 // 1. Associations
                 // Join with the Address entity (correct field name for address relationship)
-                criteria.createAlias("address", "a") // Join Address table
+                Criteria criteria1 = session.createCriteria(Person.class).createAlias("address", "a") // Join Address table
                         .add(Restrictions.eq("a.city", "Delhi")); // Filter by city 'Delhi'
 
+                
+                List results1 = criteria1.list();
+                // Execute the query and retrieve the results
+                System.out.println("results1 "+results1);
+                
                 // 2. Restrictions
                 // Add a restriction on the date of birth (DOB)
                 Date startDate = new Date(80, 0, 1);
                 Date endDate = new Date(90, 0, 1);
-                criteria.add(Restrictions.between("dob", startDate, endDate));
+                Criteria criteria2 = session.createCriteria(Person.class).add(Restrictions.between("dob", startDate, endDate));
 
+                List results2 = criteria2.list();
+                // Execute the query and retrieve the results
+                System.out.println("results2 "+results2);
+                
                 // 3. Ordering
                 // Order the results by name in descending order
-                criteria.addOrder(Order.desc("name"));
-
+                Criteria criteria3 = session.createCriteria(Person.class).addOrder(Order.desc("name"));
+                List results3 = criteria3.list();
+                // Execute the query and retrieve the results
+                System.out.println("results3 "+results3);
+                
                 // 4. Projections
                 // Select the name and date of birth properties and return a list of custom objects
-                criteria.setProjection(Projections.projectionList()
+                Criteria criteria4 = session.createCriteria(Person.class).setProjection(Projections.projectionList()
                         .add(Projections.property("name"), "name")
                         .add(Projections.property("dob"), "dob"))
                         .setResultTransformer(Transformers.aliasToBean(PersonSummary.class));
 
+                List<PersonSummary> results4 = criteria4.list();
+                
+                // Process the results (can be done in any suitable way)
+                results4.forEach(new Consumer<PersonSummary>() {
+					public void accept(PersonSummary ps) {
+						System.out.println("results4 "+ ps);
+					}
+				});
+                
                 // 5. Aggregation
                 // Count the number of persons in the result set
-                criteria.setProjection(Projections.rowCount());
-
+                Criteria criteria5 = session.createCriteria(Person.class).setProjection(Projections.rowCount());
+                List results5 = criteria5.list();
                 // Execute the query and retrieve the results
-                List<PersonSummary> results = criteria.list();
+                System.out.println("results5 "+ results5);
 
-                // Process the results (can be done in any suitable way)
-                results.forEach(personSummary -> System.out.println(personSummary));
+               session.close();
 
             } catch (Exception e) {
                 if (tx != null) tx.rollback();
